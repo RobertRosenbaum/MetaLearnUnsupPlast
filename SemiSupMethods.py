ae4e8c152283e14d64c9e4a2f3fb0d719d6e08cf
RobertRosenbaum <rjrosenbaum@gmail.com>
2023-04-22 10:29:15 -0400

    Created Semisupervised method. Added file for supervised modules.

--- SemiSupMethods.py
+++ SemiSupMethods.py
@@ -20,46 +20,123 @@ import numpy as np
 # and uses the MSE (L2) loss function.
 
 class SemiSupOneLayerMSE(object):
-    def __init__(self, NumItUnsup, NumItSup, lr, mUnsup, mSup, mTestSup):
+    def __init__(self, SupModule, SupLossFun, SupOptimizer, NumItUnsup, NumItSup):
+
+        # Module for creating supervised agents
+        # Calling:
+        # model = SupModule()
+        # should return an appropriate model for supervised learning
+        self.SupModule = SupModule
+
+        # An optimizer for supervised learning
+        # e.g., SupOptimizer = torch.optim.Adam
+        self.SupOptimizer = SupOptimizer
+
+        # A loss function for supervised learning
+        # e.g., SupLossFun = nn.CrossEntropyLoss()
+        self.SupLossFun = SupLossFun
+
         # Number of iterations for unsupervised learning
         self.NumItUnsup = NumItUnsup
 
         # Number of iterations for supervised learning
         self.NumItSup = NumItSup
 
-        # Learning rate for supervised learning
-        self.lr = lr
-
         # Loss and accuracy curves
-        self.TrainLossCurve = np.zeros(self.NumItSup)
-        self.TrainAccuracyCurve = np.zeros(self.NumItSup)
+        self.TrainLossCurve = torch.zeros(self.NumItSup)
+        self.TrainAccuracyCurve = torch.zeros(self.NumItSup)
+        self.MeanTestLoss = -1
 
     # Function to simulate one generation of a population of agents.
     # This corresponds to iterating the unsupervised learning rule.
-    def UnsupLifetime(self, Population, X):
-        # Number of data points
-        NumData = X.shape[1]
-
-        # Initialize data counter
-        iSampleUnsup = 0
-
-        # For each agent and each iteration
+    def UnsupLifetime(self, Population, unsup_loader):
         for j in range(len(Population)):
+            UnsupIterator = iter(unsup_loader)
             for k in range(self.NumItUnsup):
-
-                # Sample data for unsup learning
-                if (iSampleUnsup + self.mUnsup) >= NumData:
-                    iSampleUnsup = 0
-                    Ishuffle = np.random.permutation(NumData)
-                    X = X[:, Ishuffle]
-                Isample = range(iSampleUnsup, iSampleUnsup + self.mUnsup)
-                Xsample = X[:, Isample]
-                iSampleUnsup = iSampleUnsup + self.mUnsup
+                try:
+                    X,_=next(UnsupIterator)
+                except:
+                    UnsupIterator = iter(unsup_loader)
+                    X, _ = next(UnsupIterator)
 
                 # Apply network to a batch of inputs
-                V = Population[j].Apply(Xsample)
+                V = Population[j].Apply(X)
 
                 # Update params
                 Population[j].UpdateParams()
+
+    # Function to compute meta-loss using the current meta-params in population.
+    # This corresponds to performing a supervised learning loop
+    # and then computing error rate on a test data set.
+    def ComputeMetaLoss(self, Population, train_loader, test_loader):
+        self.TrainLossCurve = np.zeros(self.NumItSup)
+        self.TrainAccuracyCurve = np.zeros(self.NumItSup)
+        self.MeanTestLoss = 0
+
+
+        TrainIterator = iter(train_loader)
+        TestIterator = iter(test_loader)
+
+        # For each agent and each iteration
+        losses = torch.zeros(len(Population))
+        ErrorRate = torch.zeros(len(Population))
+        for j in range(len(Population)):
+
+            # Initialize new supervised model and
+            # optimizer
+            SupModel = self.SupModule()
+            optimizer = self.SupOptimizer(SupModel.parameters())
+
+
+            # Perform supervised learning
+            for k in range(self.NumItSup):
+                try:
+                    Xtrain,Ytrain = next(TrainIterator)
+                except:
+                    TrainIterator = iter(train_loader)
+                    Xtrain, Ytrain = next(TrainIterator)
+
+                # Pre-process with agent
+                V = Population[j].Apply(Xtrain)
+
+                # Get output and loss
+                Yhat = SupModel(V)
+                Loss = self.SupLossFun(Yhat, Ytrain)
+
+
+
+                # Update using optimizer passed in
+                optimizer.zero_grad()
+                Loss.backward()
+                self.SupOptimizer().step()
+
+                # Store pop avg train accuracy and loss
+                with torch.no_grad():
+                    Guesses = np.argmax(Yhat, axis=1)
+                    self.TrainAccuracyCurve[k] += np.mean((Guesses == Ytrain)) / len(Population)
+                    self.TrainLossCurve[k] += Loss.item()/len(Population)
+
+
+            with torch.no_grad():
+
+                # Get test data
+                try:
+                    Xtest,Ytest = next(TestIterator)
+                except:
+                    TestIterator = iter(test_loader)
+                    Xtest, Ytest = next(TestIterator)
+
+                # Pre-process with agent
+                V = Population[j].Apply(Xtest)
+
+                # Get output and loss
+                Yhat = SupModel(V)
+                self.MeanTestLoss += self.SupLossFun(Yhat, Ytest)/len(Population)
+
+                # Compute error rate
+                Guesses = np.argmax(Yhat, axis=0)
+                Accuracy = np.mean((Guesses == Ytest))
+                ErrorRate[j] = 1-Accuracy
+        return ErrorRate
 
 
